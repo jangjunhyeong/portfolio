9d021030acdc715211ab3e5d3363e219f79fbca5
jangjunhyeong <wkdwnsgud1@gmail.com>
2022-05-30 17:32:24 +0900

    issue listup

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+const root = document.getElementById('root');
+console.log(root);
+
+/* issue list up
+1. 그리드 방식으로 코넬식 노트 만들기
+2. 노트줄을 어떻게 createElement로 만들것인가
+3. 만약에 못한다면 그냥 div을 무식하게 만들어서 제어해보기
+4. 스크롤 방식일 때 코넬식 노트가 적절할까?
+5. 가로줄이 없는 코넬식 노트 방식으로 하는게 더 적절해보인다.
+6. 가독성문제도 생길 수 있고, figma에서 가상 그리드 방식으로 배치한 뒤에 마크업하자
+
+
+*/
